e269958aac89a0c7f3f96a64098b6bf0031e3963
Roy Penrod <roy@roypenrod.com>
2016-04-11 17:03:09 -0500

    added dynamic titles to home, help, and about static pages

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -4,16 +4,19 @@ class StaticPagesControllerTest < ActionController::TestCase
   test "should get home" do
     get :home
     assert_response :success
+    assert_select "title", "Home | ping -- The Microblog for Web Professionals"
   end
 
   test "should get help" do
     get :help
     assert_response :success
+    assert_select "title", "Help | ping -- The Microblog for Web Professionals"
   end
 
   test "should get about" do
   	get :about
   	assert_response :success
+  	assert_select "title", "About | ping -- The Microblog for Web Professionals"
   end
 
 end
