c2756d53cf81198fa21be92ea76c1efd27244c62
Roy Penrod <roy@roypenrod.com>
2016-04-12 10:22:22 -0500

    added full_title app helper method

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+
+	# returns the title on a per-page basis
+	def full_title (page_title = '')
+		base_title = 'ping -- the microblog for web developers'
+
+		if page_title.empty?
+			base_title
+		else
+			page_title + ' @ ' + base_title
+		end
+	end
 end
--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -4,25 +4,25 @@ class StaticPagesControllerTest < ActionController::TestCase
   test "should get home" do
     get :home
     assert_response :success
-    assert_select "title", "home @ ping -- the microblog for web professionals"
+    assert_select "title", "ping -- the microblog for web developers"
   end
 
   test "should get help" do
     get :help
     assert_response :success
-    assert_select "title", "help @ ping -- the microblog for web professionals"
+    assert_select "title", "help @ ping -- the microblog for web developers"
   end
 
   test "should get about" do
   	get :about
   	assert_response :success
-  	assert_select "title", "about @ ping -- the microblog for web professionals"
+  	assert_select "title", "about @ ping -- the microblog for web developers"
   end
 
   test "should get contact" do
   	get :contact
   	assert_response	:success
-  	assert_select "title", "contact @ ping -- the microblog for web professionals"
+  	assert_select "title", "contact @ ping -- the microblog for web developers"
 
   end
 
